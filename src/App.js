5fdc1609e2e96eb56a82423314d090194bc18ea3
West <agus2kcoin@outlook.com>
2022-12-30 18:43:33 -0300

    Feature context api

--- src/App.js
+++ src/App.js
@@ -22,10 +22,12 @@ import {
   ColorMapping,
   Editor,
 } from './pages';
+
+import { useStateContext } from './context/ContextProvider';
 import './App.css';
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,8 +7,11 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { links } from '../data/dummy';
 
+import { useStateContext } from '../context/ContextProvider';
+
 const Sidebar = () => {
-  const activeMenu = true;
+  const { activeMenu, setActiveMenu } = useStateContext();
+
   const activeLink =
     'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink =
@@ -21,7 +24,9 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => {}}
+              onClick={() => {
+                setActiveMenu(false);
+              }}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware />
@@ -30,7 +35,9 @@ const Sidebar = () => {
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => {}}
+                onClick={() => {
+                  setActiveMenu((prevActiveMenu) => !prevActiveMenu);
+                }}
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
