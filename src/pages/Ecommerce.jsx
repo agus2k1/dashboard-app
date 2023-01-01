134213e958f9cb228c1f1777fa8a4d10e51ed2b9
West <agus2kcoin@outlook.com>
2023-01-01 13:42:04 -0300

    Feature ecommerce page

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -11,13 +11,12 @@ import {
 import { useStateContext } from '../context/ContextProvider';
 
 const Ecommerce = () => {
-  const {} = useStateContext();
-
   return (
-    <div className="mt-12">
-      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+    <main className="mt-12">
+      {/* Earnings section */}
+      <section className="flex flex-wrap lg:flex-nowrap justify-center">
         {/* Top Hero */}
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+        <section className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
@@ -33,9 +32,9 @@ const Ecommerce = () => {
               size="md"
             />
           </div>
-        </div>
+        </section>
         {/* Cards */}
-        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+        <section className="flex m-3 flex-wrap justify-center gap-1 items-center">
           {earningData.map((item) => (
             <div
               key={item.title}
@@ -50,16 +49,86 @@ const Ecommerce = () => {
               </button>
               <p className="mt-3">
                 <span className="text-lg font-semibold">{item.amount}</span>
-                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                <span className={`text-sm ${item.pcColor} ml-2`}>
                   {item.percentage}
                 </span>
               </p>
               <p className="text-sm text-gray-400 mt-1">{item.title}</p>
             </div>
           ))}
-        </div>
-      </div>
-    </div>
+        </section>
+      </section>
+      {/* Revenue Updates Section */}
+      <section className="flex gap-10 flex-wrap justify-center">
+        <section className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+          {/* Title */}
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+          {/* Data + Charts */}
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              {/* Budget */}
+              <div>
+                <p>
+                  <span className="text-3xl font-semibold">$47,293</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                    23%
+                  </span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+              {/* Expense */}
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl font-semibold">$23,763</span>
+                </p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+              {/* Line Chart */}
+              <div className="mt-5">
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkLine"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                />
+              </div>
+              {/* Download Report Button */}
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+            <div>
+              <Stacked width="320px" height="360px" />
+            </div>
+          </div>
+        </section>
+      </section>
+    </main>
   );
 };
 
