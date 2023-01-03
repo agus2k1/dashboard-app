0ab8eb83ae63995f0ba56f132a72178066ca288e
West <agus2kcoin@outlook.com>
2023-01-03 15:45:44 -0300

    Feature pie chart

--- src/components/Charts/Pie.jsx
+++ src/components/Charts/Pie.jsx
@@ -1,7 +1,67 @@
 import React from 'react';
 
-const Pie = () => {
-  return <div>Pie</div>;
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  Inject,
+  AccumulationLegend,
+  PieSeries,
+  AccumulationTooltip,
+  AccumulationDataLabel,
+} from '@syncfusion/ej2-react-charts';
+
+import { pieChartData } from '../../data/dummy';
+
+import { Header } from '../../components';
+
+import { useStateContext } from '../../context/ContextProvider';
+
+const Doughnut = ({ id, data, legendVisiblity, height }) => {
+  const { currentMode } = useStateContext();
+
+  return (
+    <AccumulationChartComponent
+      id={id}
+      height={height}
+      legendSettings={{ visible: legendVisiblity, background: 'white' }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+    >
+      <Inject
+        services={[
+          AccumulationLegend,
+          PieSeries,
+          AccumulationTooltip,
+          AccumulationDataLabel,
+        ]}
+      />
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective
+          name="Sale"
+          dataSource={data}
+          xName="x"
+          yName="y"
+          innerRadius="40%"
+          startAngle={0}
+          endAngle={360}
+          radius="70%"
+          explode
+          explodeOffset="10%"
+          explodeIndex={2}
+          dataLabel={{
+            visible: true,
+            name: 'text',
+            position: 'Inside',
+            font: {
+              fontWeight: '600',
+              color: '#fff',
+            },
+          }}
+        />
+      </AccumulationSeriesCollectionDirective>
+    </AccumulationChartComponent>
+  );
 };
 
-export default Pie;
+export default Doughnut;
