fd1c08e342dc71f9e1afb235cf85b317b2fc1e08
West <agus2kcoin@outlook.com>
2023-01-04 16:44:25 -0300

    Feature financial chart

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,7 +1,69 @@
 import React from 'react';
 
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  HiloSeries,
+  Tooltip,
+  DateTime,
+  Zoom,
+  Logarithmic,
+  Crosshair,
+} from '@syncfusion/ej2-react-charts';
+
+import {
+  FinancialPrimaryXAxis,
+  FinancialPrimaryYAxis,
+  financialChartData,
+} from '../../data/dummy';
+
+import { Header } from '../../components';
+
+import { useStateContext } from '../../context/ContextProvider';
+
 const Financial = () => {
-  return <div>Financial</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header title="Financial" category="Chart" />
+      <div className="w-full">
+        <ChartComponent
+          id="charts"
+          primaryXAxis={FinancialPrimaryXAxis}
+          primaryYAxis={FinancialPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true, shared: true }}
+          crosshair={{ enable: true, lineType: 'Vertical', line: { width: 0 } }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+        >
+          <Inject
+            services={[
+              HiloSeries,
+              Tooltip,
+              DateTime,
+              Logarithmic,
+              Crosshair,
+              Zoom,
+            ]}
+          />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={financialChartData}
+              xName="x"
+              yName="low"
+              name="Apple Inc"
+              type="Hilo"
+              low="low"
+              high="high"
+            />
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
 };
 
 export default Financial;
